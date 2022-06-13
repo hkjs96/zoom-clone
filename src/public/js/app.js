813be8ab61fdc7d3661a7c7b247dc2364a57cf72
hkjs96 <hkjs96@naver.com>
2022-06-13 14:23:42 +0900

    1.7 Nicknames part One

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,40 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 // app.js 의 socket은 서버로의 연결을 의미
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+    const msg = { type, payload };
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server O");
 })
 
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 })
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server X");
 })
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 10000);
+
+function handleSubmit(event) {
+    event.preventDefault(); // 기본 이벤트 차단
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    input.value = "";
+}
+
+function handleNickSummit(event) {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSummit);
--- src/server.js
+++ src/server.js
@@ -25,17 +25,33 @@ const server = http.createServer(app);
 // 같은 포트(3000)를 사용하기 위해서 websocket의 인스턴스로 httpserver 인스턴스를 인수로 넣음.
 const wss = new Websocket.Server({ server });
 
+// 배열에 받아 다른 socket에 내용 전달 가능?
+const sockets = [];
+
 // server.js에서 여기 handleConnection 의 socket 은 연결된 브라우저를 뜻함.
 // socket? 연결된 어떤 사람인 것, 연결된 브라우와의 연락(contact) 라인
 // socket 은 나(서버)와 브라우져 사이의 연결 역할을 수행
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser O ");
     socket.on("close", () => console.log("Disconnected from the Browser X"));
     socket.on("message", (message) => {
-        console.log(message.toString());
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
     });
-    socket.send("hello!!!");
 });
 
 // http 서버에서 access를 하려는 것, http 서버 위에서 ws를 구동
 server.listen(3000, handleListen);
+
+
+
+
+// {
+//     type: "message",
+//     payload: "안녕하세요!"
+// }
+
+// {
+//     type: "nickname",
+//     payload: "지수"
+// }
